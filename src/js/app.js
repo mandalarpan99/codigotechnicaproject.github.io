7b4452d071f5b952d02fab64cbb8c5ebd112743d
pritesh <royalcoders1@gmail.com>
2022-06-11 01:11:09 -0700

    scroll to top funtion added

--- src/js/app.js
+++ src/js/app.js
@@ -64,3 +64,24 @@ const boxAnimation = new IntersectionObserver((e)=>{
     threshold:1
 }) 
 boxAnimation.observe(servicesContainer);
+
+
+// scroll to top
+
+
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
